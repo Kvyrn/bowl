0b8ab12924c8697d1d681c191d3d6a6dc09244ec
Treesoid <70215358+Treesoid@users.noreply.github.com>
2023-03-18 07:17:59 +0100

    three-d

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+mod main;
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    main::main();
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,52 @@
-mod diags;
+use three_d::*;
 
-use bevy::prelude::*;
-use diags::{DiagsPlugin, DiagsText};
+pub fn main() {
+    let window = Window::new(WindowSettings {
+        title: "Bowl!".to_string(),
+        ..Default::default()
+    })
+    .unwrap();
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(DiagsPlugin)
-        .add_startup_system(setup)
-        .add_system(rotate)
-        .add_system(toggle_diags)
-        .run();
-}
+    let context = window.gl();
 
-#[derive(Component)]
-struct Shape;
+    let mut camera = Camera::new_perspective(
+        window.viewport(),
+        vec3(0.0, 0.0, 2.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        1000.0,
+    );
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 3.0),
-        ..default()
-    });
-    commands.spawn(PointLightBundle {
-        transform: Transform::from_xyz(3.0, 10.0, 3.0),
-        ..default()
-    });
+    let positions = vec![
+        vec3(0.5, -0.5, 0.0),
+        vec3(-0.5, -0.5, 0.0),
+        vec3(0.0, 0.5, 0.0),
+    ];
+    let colors = vec![
+        Color::new(255, 0, 0, 255),
+        Color::new(0, 255, 0, 255),
+        Color::new(0, 0, 255, 255),
+    ];
+    let cpu_mesh = CpuMesh {
+        positions: Positions::F32(positions),
+        colors: Some(colors),
+        ..Default::default()
+    };
 
-    let material = materials.add(StandardMaterial {
-        base_color: Color::GREEN,
-        ..default()
-    });
-    let mesh = meshes.add(shape::Cube::default().into());
+    let mut model = Gm::new(Mesh::new(&context, &cpu_mesh), ColorMaterial::default());
+    model.set_animation(|time| Mat4::from_angle_y(radians(time * 0.001)));
 
-    commands.spawn((
-        PbrBundle {
-            mesh,
-            material,
-            ..default()
-        },
-        Shape,
-    ));
-}
+    window.render_loop(move |frame_input| {
+        camera.set_viewport(frame_input.viewport);
+        model.animate(frame_input.accumulated_time as f32);
 
-fn rotate(mut query: Query<&mut Transform, With<Shape>>) {
-    for mut transform in query.iter_mut() {
-        transform.rotation *= Quat::from_rotation_y(0.01);
-    }
-}
+        frame_input
+            .screen()
+            .clear(ClearState::color_and_depth(0.05, 0.0, 0.1, 1.0, 1.0))
+            .render(&camera, &model, &[]);
 
-fn toggle_diags(mut query: Query<&mut Visibility, With<DiagsText>>, input: Res<Input<KeyCode>>) {
-    if input.just_pressed(KeyCode::F9) {
-        for mut visibility in query.iter_mut() {
-            if matches!(*visibility, Visibility::Hidden) {
-                *visibility = Visibility::Inherited;
-            } else {
-                *visibility = Visibility::Hidden;
-            }
-        }
-    }
+        FrameOutput::default()
+    })
 }
