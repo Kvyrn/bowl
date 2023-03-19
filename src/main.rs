0b1a85f70876ac594d88899c5409ca2908ff7477
Treesoid <70215358+Treesoid@users.noreply.github.com>
2023-03-19 13:53:46 +0100

    use main for lib

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-#[cfg(not(target_arch = "wasm32"))]
 mod simulation;
-#[cfg(not(target_arch = "wasm32"))]
 mod ui;
 
 use crate::simulation::{Dim, SimulationContext};
@@ -99,4 +97,15 @@ pub fn main() {
 
         FrameOutput::default()
     });
+}
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    main();
+    Ok(())
 }
