5e1dcd6e3deb8d35ea7c40c7be42e8c6d7e5a036
Treesoid <70215358+Treesoid@users.noreply.github.com>
2023-03-19 14:04:00 +0100

    fill level

--- src/ui.rs
+++ src/ui.rs
@@ -38,20 +38,34 @@ fn simulation_section(ui: &mut Ui, sim: &mut SimulationContext) {
         ui.strong("Size");
         ui.end_row();
 
-        ui.label("Dim 1");
+        ui.label(RichText::new("Dim 1").color(Color32::BLUE));
         ui.add(DragValue::new(&mut sim.dim1_size));
+        ui.add(
+            ProgressBar::new(sim.state.dim1_fill as f32 / sim.dim1_size as f32).fill(Color32::BLUE),
+        );
         ui.end_row();
 
-        ui.label("Dim 2");
+        ui.label(RichText::new("Dim 2").color(Color32::DARK_GREEN));
         ui.add(DragValue::new(&mut sim.dim2_size));
+        ui.add(
+            ProgressBar::new(sim.state.dim2_fill as f32 / sim.dim2_size as f32)
+                .fill(Color32::DARK_GREEN),
+        );
         ui.end_row();
 
-        ui.label("Dim 3");
+        ui.label(RichText::new("Dim 3").color(Color32::RED));
         ui.add(DragValue::new(&mut sim.dim3_size));
+        ui.add(
+            ProgressBar::new(sim.state.dim3_fill as f32 / sim.dim3_size as f32).fill(Color32::RED),
+        );
         ui.end_row();
 
-        ui.label("Dim 4");
+        ui.label(RichText::new("Dim 4").color(Color32::YELLOW));
         ui.add(DragValue::new(&mut sim.dim4_size));
+        ui.add(
+            ProgressBar::new(sim.state.dim4_fill as f32 / sim.dim4_size as f32)
+                .fill(Color32::YELLOW),
+        );
         ui.end_row();
     });
     Grid::new("simulation_use").show(ui, |ui| {
